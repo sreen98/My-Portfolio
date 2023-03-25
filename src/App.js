d1edb116a142eaff61b6ca490ffd0578a644129a
sreen98 <sreenath.premnath@gmail.com>
2023-03-25 19:52:41 +0530

    experience base setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Home from "./components/Home";
-import Projects from "./components/Projects";
+import Experience from "./components/Experience";
 import Skills from "./components/Skills";
 import { Routes, Route } from "react-router-dom";
 import Resume from "./components/Resume";
@@ -10,7 +10,7 @@ function App() {
     <main>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
+        <Route path="/experience" element={<Experience />} />
         <Route path="/contactinfo" element={<ContactInfo />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/resume" element={<Resume />} />
